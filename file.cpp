5ec2b4d48db289df40ed8c317ae0b5996d67a9bf
Zack Fielding <zackfielding@gmail.com>
2022-07-07 21:37:44 -0600

    completely re-worked - compiles - runs in infinite loop

--- file.cpp
+++ file.cpp
@@ -3,13 +3,7 @@
 #import <cstring>
 #import <cctype>
 
-//[x] create custom string cat func to deal with xyz + \t concat
-//[] debug
-//[] test multi_cat_string()
-
-typedef void(*FnPtr_get_string)(char*, const int&, std::fstream&, void(*));
 typedef void(*FnPtr_reset_string)(char*);
-typedef void(*FnPtr_multi_string_cat)(char**, const int&);
 
 void get_string(char* string, const int& stringSize, std::fstream& file, 
 		FnPtr_reset_string){
@@ -32,41 +26,31 @@ void reset_string(char* string){
 	return;
 }
 
-void extract_modify_string(char* string, const int& stringSize, 
-		std::fstream& openFile, std::fstream &saveFile, 
-		FnPtr_get_string, FnPtr_reset_string, FnPtr_multi_string_cat){ 
-
-	char xyz[]{"xyz"};
-	const int stringArraySize{3};
-	char* stringArray[stringArraySize]; //array of points to char 
-	while(openFile.peek() != '\n'){
-		*(FnPtr_get_string)(string, stringSize, openFile, FnPtr_reset_string); //returns current header string
-		stringArray[0] = string; //assing current header to char array
-		stringArray[2] = '\t'; // assign tab char -> last index
-		for(size_t w {0}; w < 3; ++w){
-			stringArray[1] = xyz[w]; //assign x/y/z char to middle char array index
-			*(FnPtr_multi_string_cat)(stringArray, stringArraySize); //pass entire char array to cat all string
-		   	saveFile << string; //pass cat string
-		}
-	}
-}
-
-void multi_string_cat(char** stringArray, const int& stringArraySize){
+void create_new_header(char* string, int stringSize){
 	
-		//concatenates string to 0-INDEXED string in char**
-		//does not return new string -> is appending to a currently existing string
-	size_t i {1}, j {0};
-	for(i ; i < stringArraySize; ++i){ //looping throuhg ptr->ptrs
-		while(stringArray[i][j] != '\0'){ //looping through indiviual strings
-			stringArray[0][j] = stringArray[i][j];	
-			++j;
-		}
-		j = 0; //reset
-	}
+	char xyz_tab[] {"xyz\t"};
 
-	stringArray[i-1][j] = '\0'; //append null terminator
+	size_t i {0};
+	for(i; i < 4; ++i){
+		string[stringSize] = xyz_tab[i];
+		++stringSize;	
+	}
+	
+	string[stringSize] = '\0';	
+
 	return;
 }
+
+int string_size(char* string){
+
+	size_t i {0};
+	int stringSize{0};
+	while(string[i] != '\0'){
+		++stringSize;	
+	}
+	return stringSize;
+}
+
 
 int main(){
 	
@@ -89,9 +73,14 @@ int main(){
 
 	while(!openFile.eof()){
 		get_string(string, stringSize, openFile, reset_string); //read string from file
-		if(std::strcmp(string, comp_string) == 0) //if string matches comp_string
-			extract_modify_string(string, stringSize, openFile, saveFile,
-					get_string, reset_string, multi_string_cat);
+		if(std::strcmp(string, comp_string) == 0){ //if string matches comp_string
+			while(openFile.peek() != '\n'){
+				get_string(string, stringSize, openFile, reset_string); // read string
+				for(size_t i {0}; i < 3; ++i){
+					create_new_header(string, string_size(string)); 
+					saveFile << string; // write to new file
+				}
+			}	
 		}		
 	}
 
