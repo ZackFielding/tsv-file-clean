f48e821afec5600fe321e53b602c460db6fee222
Zack Fielding <zackfielding@gmail.com>
2022-07-09 10:25:51 -0600

    adjusted header read and write working

--- file.cpp
+++ file.cpp
@@ -10,7 +10,6 @@ void get_string(char* string, const int& stringSize, std::fstream& file,
 	reset_string(string);
 	size_t index {0};
 	while(!std::isspace(file.peek())){
-		std::cout << "file peek == " << file.peek() << std::endl;
 		string[index] = file.get();
 		++index;	
 	}
@@ -23,16 +22,13 @@ void get_string(char* string, const int& stringSize, std::fstream& file,
 }
 
 void clear_spaces(std::fstream& file, bool headerRowFound){
-	// consume tab or ws -> if new line char, keep it (need it for main logic)
-	// if header bool ==  false -> consume any white space (incl. new line char)
-	// if header bool == true -> do not consume new line char but all other ws chars
 	
 	if(!headerRowFound){
 		do{
 			file.get();
 		}while(std::isspace(file.peek()));	
 	}else{
-		while(file.peek() != '\n')
+		while(std::isspace(file.peek()) && file.peek() != '\n')
 			file.get();
 	}
 }
@@ -46,19 +42,15 @@ void reset_string(char* string){
 	return;
 }
 
-void create_new_header(char* string, int stringSize){
+void create_new_header(char* string, int stringSize, 
+		std::fstream& writeFile){
 	
-	char xyz_tab[] {"xyz\t"};
-
-	size_t i {0};
-	for(i; i < 4; ++i){
-		string[stringSize] = xyz_tab[i];
-		++stringSize;	
-	}
-	
-	string[stringSize] = '\0';	
-
-	return;
+	char xyz_tab[] {"xyz"};
+	for(size_t a {0}; a < 3; ++a){
+		string[stringSize] = xyz_tab[a]; //append x OR y OR z
+		string[stringSize+1] = '\t'; //append tab char (.tsv file)
+		writeFile << string; // write to current position in new file
+	}	
 }
 
 int string_size(char* string){
@@ -91,20 +83,21 @@ int main(){
 		
 	const int stringSize {50};
 	char string [stringSize] {'\0'}, comp_string [] {"MARKER_NAMES"};
+	int loop_counter{0};
 
 	while(!openFile.eof()){
 		get_string(string, stringSize, openFile, reset_string); //read string from file
+		std::clog << "String: " << string << "\n";
 		clear_spaces(openFile, false);
 		if(std::strcmp(string, comp_string) == 0){ //if string matches comp_string
 			while(openFile.peek() != '\n'){
+				std::clog << "inner while loop.\n";
 				get_string(string, stringSize, openFile, reset_string); // read string
 				clear_spaces(openFile, true);
-				for(size_t i {0}; i < 3; ++i){
-					create_new_header(string, string_size(string)); 
-					saveFile << string; // write to new file
-				}
+				create_new_header(string, string_size(string), saveFile); 
 			}	
-		}		
+			break;
+		}
 	}
 
 	openFile.close();
