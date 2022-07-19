6aa9815b7138c4e7ba3f528ffc7be2631b7a45f5
Zack Fielding <zackfielding@gmail.com>
2022-07-19 16:39:21 -0600

    working kinematic file read & write - GRF still in progress

--- file.cpp
+++ file.cpp
@@ -63,24 +63,41 @@ int string_size(char* string){
 	return stringSize;
 }
 
+void create_grf_headers(std::fstream& saveFile){
+
+	char grf_ar[]{"grf n"}; // 4
+	char xyz[]{"xyz"};
+	for(size_t i {0}; i < 3; ++i){
+		grf_ar[4] = xyz[i];
+		saveFile.write(grf_ar, sizeof grf_ar);
+		if(i < 2)
+			saveFile.put('\t');
+	}
+	saveFile.put('\n');
+}
+
 int main(){
 	
-	std::fstream openFile; // original file
+	std::fstream openFile, saveFile; // creating fstream objects
 	openFile.open("P08_BS_01_pro.tsv", std::ios_base::in);
-	
-	std::fstream saveFile; // new (cleaned) file
 	saveFile.open("P08_kinematic.tsv", std::ios_base::out);
 
 	if(!openFile || !saveFile){
-		if(!openFile)
-			std::cerr << "Error opening original file.\n";
-		else
-			std::cerr << "Error opening to-write-to file.\n";
-		return 1;
+		if(!openFile){
+			std::cerr << "Error opening original kinematic file.\n";
+			if(saveFile)
+				saveFile.close();
+			return 1;
+		}else{
+			std::cerr << "Error opening original to-write-to file.\n";
+			if(openFile)
+				openFile.close();
+			return 1;
+		}
 	}
-		
+			
 	const int stringSize {50};
-	char string [stringSize] {'\0'}, comp_string [] {"MARKER_NAMES"};
+	char string [stringSize] {'\0'}, comp_string [20] {"MARKER_NAMES"};
 	int loop_counter{0};
 	int num_of_tabs {0};
 
@@ -97,13 +114,18 @@ int main(){
 			}	
 
 			std::clog << "Header created.\n";
+			saveFile.put(openFile.get()); // put new line char -> new file
+
+			// consume all contents on this row (unnecessary text)
+			while(openFile.peek() != '\n')
+				openFile.get();
 			openFile.get(); // consume new line char
 
-			for(size_t a {0}; a < 5; ++a){
-				if (a < num_of_tabs)
-					saveFile.get(*openFile.rdbuf(), '\t');	
+			while(!openFile.eof()){
+				openFile.get(*saveFile.rdbuf(), '\t');
+				openFile.get(); // consume tab char
+				saveFile.put('\t'); // put tab char
 			}
-
 			std::clog << "End of file reached.\n";
 			}
 		}
@@ -112,9 +134,65 @@ int main(){
 	saveFile.close();
 	
 	if(!openFile.is_open())
-		std::clog << "Read only file closed.\n";
+		std::clog << "Read only kinematic file closed.\n";
 	if(!saveFile.is_open())
-		std::clog << "Write only file closed.\n";
+		std::clog << "Write only kinematic file closed.\n";
+
+	// similar approach for ground reaction force files
+	
+	openFile.open("P08_BS_01_pro_f_1.tsv", std::ios_base::in);
+	saveFile.open("P08_grf_south.tsv", std::ios_base::out);
+
+	if(!openFile || !saveFile){
+		if(!openFile){
+			std::cerr << "Error opening original GRF file.\n";
+			if(saveFile)
+				saveFile.close();
+			return 1;
+		}else{
+			std::cerr << "Error opening original GRF file.\n";
+			if(openFile)
+				openFile.close();
+			return 1;
+		}
+	}
+
+	reset_string(comp_string); // reset comp string
+	comp_string = "FORCE_PLATE_WIDTH";
+
+	while(!openFile.eof()){
+		get_string(string, stringSize, openFile, reset_string); //read string from file
+		clear_spaces(openFile, false);
+		if(std::strcmp(string, comp_string) == 0){ //if string matches comp_string
+			while(openFile.peek() != '\n')
+				openFile.get();
+			openFile.get(); // consume new line char
+
+			create_grf_header(saveFile); // write grf headers to new file
+
+			int tab_counter {0};
+			while(!openFile.eof()){
+				while(tab_counter <= 3){
+					openFile.get(*saveFile.rdbuf(), '\t');
+					openFile.get(); // consume tab char
+					saveFile.put('\t'); // put tab char
+					++tab_counter;
+				}
+				while(openFile.peek() != '\n')
+					openFile.get();
+				saveFile.put(openFile.get()); // put new line char -> new file
+			}
+			std::clog << "End of file reached.\n";
+			}
+		}
+
+	openFile.close();
+	saveFile.close();
+	
+	if(!openFile.is_open())
+		std::clog << "Read only GRF file closed.\n";
+	if(!saveFile.is_open())
+		std::clog << "Write only GRF file closed.\n";
 
 	return 0;
 }
